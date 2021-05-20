2b0260d3bf415f523f710e070551a1be207079eb
unknown <bruno.oliveira@redspark.io>
2021-05-19 21:13:57 -0300

    apenas o jar

--- src/main/java/br/com/meumetro/notification/restfull/RestConfig.java
+++
@@ -1,8 +0,0 @@
-package br.com.meumetro.notification.restfull;
-
-public class RestConfig {
-
-	public static String BASE_URL = "https://meumetro.herokuapp.com/";
-	public static final String BASIC_AUTHENTICATE_PASSWORD = "e871f5a73c097ec22f27ebe917ed9f87";
-
-}
